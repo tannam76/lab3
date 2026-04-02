df3237df029eeed19644b977dd505fd7d3efca38
tannam76 <148177343+tannam76@users.noreply.github.com>
2026-04-02 07:49:02 +0700

    Update SchoolmanagerApplication.java

--- src/main/java/com/example/schoolmanager/SchoolmanagerApplication.java
+++ src/main/java/com/example/schoolmanager/SchoolmanagerApplication.java
@@ -1,11 +1,12 @@
-package com.example.studentmanager;
+package com.example.schoolmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentmanagerApplication {
+public class SchoolmanagerApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(StudentmanagerApplication.class, args);
+        SpringApplication.run(SchoolmanagerApplication.class, args);
     }
 }
