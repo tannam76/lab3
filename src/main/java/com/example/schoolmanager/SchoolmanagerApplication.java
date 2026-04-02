eca08cf756d610de8310c942602334538f5e567b
tannam76 <148177343+tannam76@users.noreply.github.com>
2026-04-02 07:42:03 +0700

    Update SchoolmanagerApplication.java

--- src/main/java/com/example/schoolmanager/SchoolmanagerApplication.java
+++ src/main/java/com/example/schoolmanager/SchoolmanagerApplication.java
@@ -1,14 +1,11 @@
-# Port
 server.port=${PORT:8080}
 
-# Database (Render PostgreSQL)
 spring.datasource.url=jdbc:postgresql://dpg-d6mokcftskes73e1pd90-a:5432/school_kbjz
 spring.datasource.username=school_kbjz_user
 spring.datasource.password=jdhjIHKVisnAteB9Etfc5f32LgGNr2Ln
 
 spring.datasource.driver-class-name=org.postgresql.Driver
 
-# JPA / Hibernate
 spring.jpa.show-sql=true
 spring.jpa.hibernate.ddl-auto=update
 spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.PostgreSQLDialect
